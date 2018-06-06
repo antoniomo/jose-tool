882c86b01a02b52c3f8505e67b88f57d7400a43a
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2018-06-06 14:51:36 +0300

    jwk options struct

--- cmd/jwk/generate.go
+++ cmd/jwk/generate.go
@@ -13,15 +13,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	n             int
-	kty           string
-	signing       string
-	kidFormat     string
-	publicOutput  string
-	privateOutput string
-)
-
 // Generate ...
 var generate = &cobra.Command{
 	Use:   "generate [options]",
@@ -31,44 +22,44 @@ var generate = &cobra.Command{
 }
 
 func generateRun(cmd *cobra.Command, args []string) {
-	if n < 1 {
+	if opt.n < 1 {
 		fmt.Println("error: n must be at least 1")
 		return
 	}
 
 	// Normalize
-	kty = strings.ToUpper(kty)
-	signing = strings.ToUpper(signing)
+	opt.kty = strings.ToUpper(opt.kty)
+	opt.signing = strings.ToUpper(opt.signing)
 
 	var (
 		privateSet sjwk.Set
 		publicSet  sjwk.Set
 		dateSep    string
 	)
-	if kidFormat != "" {
+	if opt.kidFormat != "" {
 		dateSep = time.Now().UTC().Format("2006-01-02") + ":"
 	}
 
-	switch kty {
+	switch opt.kty {
 	case "RSA":
 		var (
 			kid string
 		)
-		for i := 0; i < n; i++ {
+		for i := 0; i < opt.n; i++ {
 			rsakey, err := rsa.GenerateKey(rand.Reader, 2048)
 			util.ExitOnError("failed generate private key", err)
 
-			if kidFormat != "" {
+			if opt.kidFormat != "" {
 				kid = dateSep + strconv.Itoa(i)
 			}
 			privateKey, err := sjwk.New(rsakey)
 			util.ExitOnError("failed to creakte JWK", err)
 
-			if signing != "" {
+			if opt.signing != "" {
 				privateKey.Set("use", "sig")
-				privateKey.Set("alg", signing)
+				privateKey.Set("alg", opt.signing)
 			}
-			if kidFormat != "" {
+			if opt.kidFormat != "" {
 				privateKey.Set("kid", kid)
 			}
 			privateKey.Set("kid", kid)
@@ -77,44 +68,44 @@ func generateRun(cmd *cobra.Command, args []string) {
 			publicKey, err := sjwk.New(&rsakey.PublicKey)
 			util.ExitOnError("failed to creakte JWK", err)
 
-			if signing != "" {
+			if opt.signing != "" {
 				publicKey.Set("use", "sig")
-				publicKey.Set("alg", signing)
+				publicKey.Set("alg", opt.signing)
 			}
-			if kidFormat != "" {
+			if opt.kidFormat != "" {
 				publicKey.Set("kid", kid)
 			}
 			publicSet.Keys = append(publicSet.Keys, publicKey)
 		}
 	default:
-		fmt.Printf("unsupported kty: %q\n", kty)
+		fmt.Printf("unsupported kty: %q\n", opt.kty)
 		return
 	}
 
-	if privateOutput == "" {
+	if opt.privateOutput == "" {
 		fmt.Println("---- Begin private keys ----")
 	}
-	util.WriteAsJSON(privateOutput, privateSet)
-	if privateOutput == "" {
+	util.WriteAsJSON(opt.privateOutput, privateSet)
+	if opt.privateOutput == "" {
 		fmt.Println("\n---- End private keys ----")
 	}
 
-	if publicOutput == "" {
+	if opt.publicOutput == "" {
 		fmt.Println("---- Begin public keys ----")
 	}
-	util.WriteAsJSON(publicOutput, publicSet)
-	if publicOutput == "" {
+	util.WriteAsJSON(opt.publicOutput, publicSet)
+	if opt.publicOutput == "" {
 		fmt.Println("\n---- End public keys ----")
 	}
 }
 
 func init() {
-	generate.Flags().IntVarP(&n, "jwks", "n", 1, "# of JWK to generate in set")
-	generate.Flags().StringVarP(&kty, "kty", "t", "RSA", "kty (RSA)")
-	generate.Flags().StringVarP(&signing, "sign", "s", "RS256", "signing method [RS256|RS384|RS512]")
-	generate.Flags().StringVarP(&kidFormat, "kidf", "i", "date:sequence", "kid format [date:sequence]")
-	generate.Flags().StringVarP(&publicOutput, "public-output", "o", "", "public keys output file")
-	generate.Flags().StringVarP(&privateOutput, "private-output", "p", "", "private keys output file")
+	generate.Flags().IntVarP(&opt.n, "jwks", "n", 1, "# of JWK to generate in set")
+	generate.Flags().StringVarP(&opt.kty, "kty", "t", "RSA", "kty (RSA)")
+	generate.Flags().StringVarP(&opt.signing, "sign", "s", "RS256", "signing method [RS256|RS384|RS512]")
+	generate.Flags().StringVarP(&opt.kidFormat, "kidf", "i", "date:sequence", "kid format [date:sequence]")
+	generate.Flags().StringVarP(&opt.publicOutput, "public-output", "o", "", "public keys output file")
+	generate.Flags().StringVarP(&opt.privateOutput, "private-output", "p", "", "private keys output file")
 
 	JWK.AddCommand(generate)
 }
--- cmd/jwk/jwk.go
+++ cmd/jwk/jwk.go
@@ -4,6 +4,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type jwkOptions struct {
+	n             int
+	kty           string
+	signing       string
+	kidFormat     string
+	publicOutput  string
+	privateOutput string
+}
+
+var opt jwkOptions
+
 // JWK root subcommand
 var JWK = &cobra.Command{
 	Use:   "jwk [options]",
